6d2cfe461760ac499b85789171bbbbec9a396c60
p0000008 <p0000008@Grinding.com.tw>
2024-02-06 14:13:57 +0800

    加入專案檔案。

---
+++ DoNothig/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DoNothig
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(536, 488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1200, 120);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Do Nothing";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft New Tai Lue", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(952, 664);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 63);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Game Start";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft New Tai Lue", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(952, 768);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 63);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quit Game";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            this.label3.MouseHover += new System.EventHandler(this.label3_MouseHover);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft New Tai Lue", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(568, 736);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(285, 63);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Main Menu";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.label4.MouseLeave += new System.EventHandler(this.label4_MouseLeave);
+            this.label4.MouseHover += new System.EventHandler(this.label4_MouseHover);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DoNothig/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace DoNothig
+{
+    public partial class Menu : Form
+    {
+        public int w = Screen.PrimaryScreen.Bounds.Width;
+        public int h = Screen.PrimaryScreen.Bounds.Height;
+        public bool main = false;
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            Location = new Point(0, 0);
+            Size = new Size(w, h);
+
+            int lw = label1.Width / 2;
+            int lh = label1.Height / 2 + 100;
+            label1.Location = new Point(w / 2 - lw, h / 2 - lh);
+
+            int gw = label2.Width / 2;
+            int gh = label2.Height / 2 - 15;
+            label2.Location = new Point(w / 2 - gw, h / 2 - gh);
+            label4.Location = new Point(w / 2 - gw, h / 2 - gh);
+
+            int qw = label3.Width / 2;
+            int qh = label3.Height / 2 - 100;
+            label3.Location = new Point(w / 2 - qw, h / 2 - qh);
+        }
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.DarkSalmon;
+        }
+
+        private void label3_MouseHover(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.DarkSalmon;
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.Gainsboro;
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.Gainsboro;
+        }
+
+        private void label4_MouseHover(object sender, EventArgs e)
+        {
+            label4.ForeColor = Color.DarkSalmon;
+        }
+
+        private void label4_MouseLeave(object sender, EventArgs e)
+        {
+            label4.ForeColor = Color.Gainsboro;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Invoke((Action)(() =>
+            {
+                label1.Text = "Don't Move Your Mouse";
+            }));
+            label2.Visible = false;
+            label3.Visible = false;
+
+            Thread move = new Thread(Tracker);
+            move.Start();
+        }
+
+        void Tracker()
+        {
+            int fx = MousePosition.X;
+            int fy = MousePosition.Y;         
+            bool check = true;
+            while (check)
+            {
+                Thread.Sleep(5);
+                int x = MousePosition.X;
+                int y = MousePosition.Y;
+
+                if (x != fx || y != fy)
+                {
+                    this.Invoke((Action)(() =>
+                    {
+                        label1.Text = "You Moved!!!";
+                        check = false;
+                        main = true;
+                        label4.Visible = true;
+                    }));
+                }
+
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Do Nothing";
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = false;
+        }
+    }
+}
